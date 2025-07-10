6dbec4a5e62b2ae7e15b2a941b71c5bbc204bb37
craftablescience <me@craftablescience.info>
2025-07-10 06:36:40 -0700

    chore: 2025-07-09, 2025-07-10

--- src/thumbnail-generator.py
+++ src/thumbnail-generator.py
@@ -112,6 +112,8 @@ STREAM_DATES = {
     "2025-06-29": "11_original",
     "2025-07-01": "11_original",
     "2025-07-03": "11_original",
+    "2025-07-09": "11_original",
+    "2025-07-10": "11_original",
 }
 
 BOX_OFFSET = 64
